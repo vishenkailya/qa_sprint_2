ed29eaa446818814f7acc651f15dbef23bc325f4
Ilya Vishnyakov <vishenkailya@gmail.com>
2023-06-21 14:53:16 +0300

    Отработаны замечания после PR

--- tests.py
+++ tests.py
@@ -15,15 +15,8 @@ class TestBooksCollector:
         collector.set_book_rating('Capital. Part 2', 10)
         assert collector.get_book_rating('Capital. Part 2') == None
 
-    @pytest.mark.parametrize('rating', [11, 15, 100])
-    def test_set_book_rating_above_10_rating_equals_1(self, rating):
-        collector = BooksCollector()
-        collector.add_new_book('Бойцовский клуб')
-        collector.set_book_rating('Бойцовский клуб', rating)
-        assert collector.get_book_rating('Бойцовский клуб') == 1
-
-    @pytest.mark.parametrize('rating', [0, -1, -10])
-    def test_set_book_rating_lower_1_rating_equals_1(self, rating):
+    @pytest.mark.parametrize('rating', [11, -1, 0])
+    def test_set_book_rating_outside_rating_equals_1(self, rating):
         collector = BooksCollector()
         collector.add_new_book('Бойцовский клуб')
         collector.set_book_rating('Бойцовский клуб', rating)
@@ -40,10 +33,10 @@ class TestBooksCollector:
         collector = BooksCollector()
         assert collector.get_book_rating('Fire Punch') == None
 
-    @pytest.mark.parametrize('rating', [11, 15, 100])
-    def test_get_book_with_specific_rating_above_range_empty_massive(self, rating):
+
+    def test_get_book_with_specific_rating_above_range_empty_massive(self):
         collector = BooksCollector()
-        assert collector.get_books_with_specific_rating(rating) == []
+        assert collector.get_books_with_specific_rating(11) == []
 
     def test_get_book_with_specific_rating_get_one_book(self):
         collector = BooksCollector()
@@ -51,27 +44,11 @@ class TestBooksCollector:
         collector.set_book_rating('1985', 7)
         assert collector.get_books_with_specific_rating(7) == ['1985']
 
-    def test_get_book_with_specific_rating_get_two_books(self):
-        collector = BooksCollector()
-        collector.add_new_book('Берсерк')
-        collector.add_new_book('Overlord')
-        collector.set_book_rating('Берсерк', 10)
-        collector.set_book_rating('Overlord', 10)
-        assert len(collector.get_books_with_specific_rating(10)) == 2
-
-    def test_get_books_rating_2_books(self):
-        collector = BooksCollector()
-        collector.add_new_book('Rage')
-        collector.add_new_book("Dante's Inferno")
-        collector.set_book_rating('Rage', 6)
-        collector.set_book_rating("Dante's Inferno", 9)
-        assert len(collector.get_books_rating()) == 2
-
     def test_add_book_in_favorites_one_book(self):
         collector = BooksCollector()
         collector.add_new_book('Бойцовский клуб')
         collector.add_book_in_favorites('Бойцовский клуб')
-        assert len(collector.get_list_of_favorites_books()) == 1
+        assert 'Бойцовский клуб' in collector.get_list_of_favorites_books()
 
     def test_add_book_in_favorites_one_book_not_in_books_list(self):
         collector = BooksCollector()
